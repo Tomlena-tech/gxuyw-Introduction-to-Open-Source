4cce31760198f1aaeba18ddd76b47716987bb168
Thomas <decourttthomas@gmail.com>
2025-08-08 10:27:26 +0200

    Add endif

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef _MAIN_H
+#define _MAIN_H 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+*Header files for the main program.
+*This file contains the function declarations and necessary includes.
+*It is included in the main.c file to provide the necessary functions.
+*/
+void print_array(int *a, int n);
+
+
+
+#endif 
+
